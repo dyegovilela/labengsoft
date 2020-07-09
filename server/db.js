a58e91cc0574ef31a16c8e58fe72d4cf1541dddc
Dyego Vilela <dyegovilela@hotmail.com>
2020-07-09 20:40:35 -0300

    alteração de layout

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ module.exports = function(){
     return connection = mysql.createConnection({
         host : 'localhost', 
         user: 'root',
-        password:'', //inserir a senha da SUA INSTALAÇÃO DO MYSQL
+        password:'0154', //inserir a senha da SUA INSTALAÇÃO DO MYSQL
         database: 'estoque' 
     });
 
